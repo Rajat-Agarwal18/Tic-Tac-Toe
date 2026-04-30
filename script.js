5f1a563f9b18bf2a0f9933c98434d64f780ca695
Rajat-Agarwal18 <931rajatagarwal@gmail.com>
2026-04-30 18:27:40 +0530

    fixed login

--- script.js
+++ script.js
@@ -14,14 +14,20 @@ var win=[
 var turnX=true;//variable to keep track of player's turn, true for player X and false for player O
 
 //get player names from local storage
-document.querySelector("button").addEventListener("click", () => {
+document.getElementById("startbtn").addEventListener("click", () => {
     const p1 = document.getElementById("player1").value;
     const p2 = document.getElementById("player2").value;
     if (p1 && p2) {
         localStorage.setItem("player1", p1);
         localStorage.setItem("player2", p2);
+        window.location.href = "index.html"; // navigate AFTER saving
+
+    }
+    else{
+        alert("Please enter both player names!");
     }
 });
+
 //retrieve player names from local storage or use default names if not set
 var ply1 = localStorage.getItem("player1") || "Player 1";
 var ply2 = localStorage.getItem("player2") || "Player 2";
